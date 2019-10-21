5d00d54694d0205dbed91361cc618677c5f89acd
William Philippe <wplb@ic.ufal.br>
2019-10-21 14:56:01 -0300

    hooks and redux

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <ul>
+      <li>ReactJs</li>
+      <li>NodeJS</li>
+      <li>NativeJs</li>
+    </ul>
+  )
+}
+
+export default App;
