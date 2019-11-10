e4632aa475bd298e5690b140691a89af29f7cf7d
William Philippe <wplb@ic.ufal.br>
2019-11-10 15:05:18 -0300

    drag and drop #1

--- rocketfy/src/App.js
+++ rocketfy/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+
 import GlobalStyled from "./styles/global";
 
+import Header from "./components/Header";
+import Board from "./components/Board";
+
 function App() {
-  return <GlobalStyled />;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
+      <GlobalStyled />
+    </DndProvider>
+  );
 }
 
 export default App;
